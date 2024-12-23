006179e9b0b05943f16d5f54fd1d04f5232029d2
jameswang000 <jameswa2@andrew.cmu.edu>
2024-12-23 15:44:27 -0500

    Changed vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/QR-Code-Component/", // Ensure this matches your repository name
   plugins: [react()],
 });
